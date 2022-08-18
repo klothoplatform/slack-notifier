753ce1d253478c29d2e84808d295b3d86d3a615a
Yuval Shavit <110620369+yuval-klotho@users.noreply.github.com>
2022-08-18 16:45:39 -0400

    handle PR synchronize (ie new commits pushed)
    When someone pushes commits to a branch that has a PR up, the PR will
    "synchronize" itself against the branch, which basically means the PR
    picks up those new pushes. When that happens, post to the PR thread.
    
    Also, some refactoring: I pulled out "check if we have a PR thread, and
    if so act on it" logic out from `handlePrClosed`, since the new
    `handlePrSynchronized` has the same boilerplate (and others will as
    well).

--- src/slack.ts
+++ src/slack.ts
@@ -1,4 +1,4 @@
-import { PullRequest, PullRequestEvent } from '@octokit/webhooks-types';
+import { PullRequest, PullRequestEvent, PullRequestSynchronizeEvent } from '@octokit/webhooks-types';
 import { WebClient } from '@slack/web-api'
 import { text } from 'express';
 import e = require('express');
@@ -29,6 +29,7 @@ export class Slack {
         let handler = new Map([
             ['opened', this.handlePrOpened],
             ['closed', this.handlePrClosed],
+            ['synchronize', this.handlePrSynchronized],
         ]).get(event.action)
         if (handler === undefined) {
             console.warn(`Don't know how to handle events of type`, event.action)
@@ -42,29 +43,57 @@ export class Slack {
         console.log('handling open event')
         let pr = event.pull_request
         let ts = await this.io.sendMessage(channel, `:pull-request: PR <${pr.html_url}|#${pr.number}: ${pr.title}> by ${event.sender.login}`)
-        await kvStore.set(this.prThreadKey(pr), ts)
+        await this.io.store.set(prThreadKey(pr), ts)
         let content = (pr.body == null) ? "No description provided" : `PR description:\n${quote(pr.body)}`
         await this.io.sendMessage(channel, content, ts)
     }
 
     private async handlePrClosed(channel: string, event: PullRequestEvent) {
         console.log('handling close event')
-        let pr = event.pull_request
-        let prevTs = await kvStore.get(this.prThreadKey(pr))
-        if (typeof prevTs === 'string') {
+        await this.onPrThread(event, async (pr, prevTs) => {
             let mergeVerb = event.pull_request.merged ? 'merged' : 'closed'
             let emoji = `:${mergeVerb}:`
             let updateTopLevelMessage = this.io.updateMessage( channel, prevTs, `${emoji} ~PR <${pr.html_url}|#${pr.number}: ${pr.title}> by ${event.sender.login}~`)
             let postToThread = this.io.sendMessage(channel, `${emoji} PR was ${mergeVerb} by ${event.sender.login}`, prevTs)
             await Promise.all([updateTopLevelMessage, postToThread])
+        })
+    }
+
+    private async handlePrSynchronized(channel: string, event: PullRequestEvent) {
+        console.log('handling sync case')
+        await this.onPrThread(event, async (pr, thread_ts) => {
+            let syncEvent = event as PullRequestSynchronizeEvent
+            let beforeShort = syncEvent.before
+            let afterShort = syncEvent.after
+            for (let i = 7; i < Math.min(syncEvent.before.length, syncEvent.after.length); ++i) {
+                beforeShort = syncEvent.before.substring(0, i)
+                afterShort = syncEvent.after.substring(0, i)
+                if (beforeShort != afterShort) {
+                    break
+                }
+            }
+            let msg = `PR updated: <${pr.html_url}/files/${syncEvent.before}..${syncEvent.after}|${beforeShort}..${afterShort}>`
+            await this.io.sendMessage(channel, msg, thread_ts)
+
+        })
+    }
+
+    private async onPrThread(event: PullRequestEvent, action: (pr: PullRequest, thread_ts: string) => Promise<void>) {
+        let pr = event.pull_request
+        let thread_ts = await this.io.store.get(prThreadKey(pr))
+        if (typeof thread_ts === 'string') {
+            await action(pr, thread_ts)
         } else {
             console.warn("no previous ts found for pr", pr)
         }
     }
+}
 
-    private prThreadKey(pr: PullRequest) {
-        return `pr_thread_${pr.url}`
-    }
+/**
+ * The key to store in `SlackIO.store`, whose value corresponds to a PR's thread in slack.
+ */
+export function prThreadKey(pr: PullRequest): string {
+    return `pr_thread_${pr.url}`
 }
 
 export interface SlackIO {

4aa85b6751a5169d4ce1300c4c1e3fcdcdc2cb21
Yuval Shavit <yuval.shavit@klo.dev>
2022-09-20 16:09:03 -0400

    handle pings
    With the introduction of the slack integration, we now fail to respond
    to the github ping until we've set up slack: we try to get the botId,
    and that causes us to get the slack creds, and that causes us to fail.
    
    We don't need the botId at that stage, so let's handle the ping
    separately. To make this work well, we also lazy-load the `Slack` obj.
    
    In the future, that will also let us hook up a secret if we want.

--- src/github-listener.ts
+++ src/github-listener.ts
@@ -6,17 +6,23 @@
 
 import express from 'express';
 import { Slack } from './slack'
-import { IssueCommentEvent, PullRequestEvent } from "@octokit/webhooks-types";
+import { IssueCommentEvent, PingEvent, PullRequestEvent } from "@octokit/webhooks-types";
 import { getChannels } from './slack-common';
 
 export const router = express.Router();
 router.use(express.json())
 
-let slack = new Slack()
+let slack: Slack | undefined
 
 router.post('/github', async (req: express.Request, res: express.Response) => {
     try {
-        let event = req.body as (PullRequestEvent | IssueCommentEvent)
+        let event = req.body as (PullRequestEvent | IssueCommentEvent | PingEvent)
+        if ('zen' in event) {
+            console.log('received ping')
+            res.send("ok")
+            return
+        }
+        slack = slack ?? new Slack()
         let botId = await slack.botId
         if (botId === undefined) {
             throw Error("Couldn't look up botId, which I need to look up what channels I'm in")
